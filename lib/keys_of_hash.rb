24026ba6fce5c49095a3c6ec8a0f7bd9886e597a
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-14 00:47:18 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,10 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     new = []
-
+    Hash.each do |keys, values|
+      if values == arguments
+        new.push(keys)
+      end
+    end
   end
 end
